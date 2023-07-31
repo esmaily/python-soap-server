ec36a2d875862f452090eb5320ecb86fbfe279b7
Jafar Esmaili <jaffar9898@gmail.com>
2023-07-31 18:50:02 +0330

    implement project core validations

---
+++ app/core_validators.py
@@ -0,0 +1,77 @@
+from spyne.error import ValidationError
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+from datetime import date, timedelta
+
+
+class Validator(ABC):
+    """Parent class of all concrete validator"""
+
+    @abstractmethod
+    def go_next(self, validator):
+        pass
+
+    @abstractmethod
+    def validate(self, request) -> Optional[str]:
+        pass
+
+
+class AbstractValidator(Validator):
+    """
+    The default chaining behavior  for validations
+    class.
+    """
+
+    _go_validator: Validator = None
+
+    def go_next(self, validator: Validator) -> Validator:
+        self._go_validator = validator
+        return validator
+
+    @abstractmethod
+    def validate(self, request: Any) -> str:
+        if self._go_validator:
+            return self._go_validator.validate(request)
+
+        return None
+
+
+class NationalCodeValidator(AbstractValidator):
+
+    def validate(self, request: Any) -> str:
+        if len(request["national_code"]) != 10:
+            raise ValidationError("national code must be 10 length")
+        else:
+            return super().validate(request)
+
+
+class BirthdayFormatValidator(AbstractValidator):
+
+    def validate(self, request: Any) -> str:
+        try:
+            date.fromisoformat(request["birthday"])
+        except ValueError:
+            raise ValidationError("Incorrect birthday format, should be YYYY-MM-DD")
+
+        return super().validate(request)
+
+
+class BirthdayCheckAdultValidator(AbstractValidator):
+
+    def validate(self, request: Any) -> str:
+        today = date.today()
+        birth = date.fromisoformat(request["birthday"])
+        age = (today - birth) // timedelta(days=365.2425)
+        if age < 18:
+            raise ValidationError("your birthday must be greater than 18")
+        else:
+            return super().validate(request)
+
+
+class ServiceLimitValidator(AbstractValidator):
+
+    def validate(self, request: Any) -> str:
+        if len(request["services"]) >= 10:
+            raise ValidationError("every customer can create 10 services")
+        else:
+            return super().validate(request)
