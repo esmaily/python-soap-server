f4dd822e4c447521254d5bb3db92e54d3ae92b0d
Jafar Esmaili <jaffar9898@gmail.com>
2023-08-12 00:23:01 +0330

    add postgres repository

--- app/repository.py
+++ app/repository.py
@@ -1,18 +1,20 @@
 import json
 
-from .models import CustomerModel, ServiceModel
+from app.schemas import CustomerSchema, ServiceSchema
+from app.models import CustomerModel, ServiceModel
+from app.db import session
 
 
-class CustomerRepository:
+class JsonDB:
     def __init__(self, db_path: str):
         self.db_path: str = db_path
 
     def get_all(self, order_by="ASK") -> list:
         with open(self.db_path) as json_file:
             data = json.load(json_file)
-            customers = list(map(lambda item: CustomerModel(**item), data["customers"]))
+            customers = list(map(lambda item: CustomerSchema(**item), data["customers"]))
             for itr in customers:
-                itr.services = list(map(lambda item: ServiceModel(**item), itr.services))
+                itr.services = list(map(lambda item: ServiceSchema(**item), itr.services))
 
         if order_by == "DESC":
             customers.reverse()
@@ -29,8 +31,8 @@ class CustomerRepository:
         if not indices:
             return None
         customer = data["customers"][indices[0]]
-        cm = CustomerModel(**customer)
-        cm.services = list(map(lambda item: ServiceModel(**item), customer["services"]))
+        cm = CustomerSchema(**customer)
+        cm.services = list(map(lambda item: ServiceSchema(**item), customer["services"]))
         return cm
 
     def store(self, customer: dict) -> object:
@@ -40,7 +42,57 @@ class CustomerRepository:
         with open(self.db_path, 'w') as outfile:
             json.dump(data, outfile)
 
-        return CustomerModel(**customer)
+        return CustomerSchema(**customer)
+
+
+class PostgresDB:
+    def __init__(self, model: CustomerModel, schema: CustomerSchema):
+        self.model = model
+        self.schema = schema
+
+    def get_all(self, order_by: str):
+        items = session.query(self.model).all()
+        serialized = list(map(lambda item: self.schema(**item.to_dict()), items))
+
+        return serialized
+
+    def store(self, data):
+        pass
+
+
+class CustomerRepository:
+    def __init__(self, db_path: str):
+        self.db_path: str = db_path
+        self.conn = PostgresDB(CustomerModel, CustomerSchema)
+
+    def get_all(self, order_by="ASK") -> list:
+        items = self.conn.get_all(order_by)
+        customers = items
+        return customers
+
+    def __raw_get_all(self):
+        with open(self.db_path) as json_file:
+            data = json.load(json_file)
+        return data
+
+    def get_by_id(self, customer_id: int) -> object:
+        data = self.__raw_get_all()
+        indices = [index for (index, item) in enumerate(data["customers"]) if item["uid"] == int(customer_id)]
+        if not indices:
+            return None
+        customer = data["customers"][indices[0]]
+        cm = CustomerSchema(**customer)
+        cm.services = list(map(lambda item: ServiceSchema(**item), customer["services"]))
+        return cm
+
+    def store(self, customer: dict) -> object:
+        data = self.__raw_get_all()
+        customer["uid"] = len(data["customers"]) + 1
+        data["customers"].append(customer)
+        with open(self.db_path, 'w') as outfile:
+            json.dump(data, outfile)
+
+        return CustomerSchema(**customer)
 
 
 class ServiceRepository:
@@ -50,7 +102,7 @@ class ServiceRepository:
     def get_all(self, order_by="ASK") -> list:
         with open(self.db_path) as json_file:
             data = json.load(json_file)
-            services = list(map(lambda item: ServiceModel(**item), data["services"]))
+            services = list(map(lambda item: ServiceSchema(**item), data["services"]))
         if order_by == "DESC":
             services.reverse()
         return services
@@ -66,7 +118,7 @@ class ServiceRepository:
         if not indices:
             return []
         customer = data["customers"][indices[0]]
-        services = list(map(lambda item: ServiceModel(**item), customer["services"]))
+        services = list(map(lambda item: ServiceSchema(**item), customer["services"]))
         return services
 
     def store(self, service: dict) -> object:
@@ -82,4 +134,4 @@ class ServiceRepository:
         with open(self.db_path, 'w') as outfile:
             json.dump(data, outfile)
 
-        return ServiceModel(**service)
+        return ServiceSchema(**service)
